cec7c9730f589853b91d16806bdc0982e8b825e5
juarezjunior256 <juarezjunior.live@gmail.com>
2021-08-22 21:45:38 -0300

    ContactsController criada

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import customers from "./app/controllers/CustomerController";
+import contacts from "./app/controllers/ContactsController";
 
 const routes = new Router();
 
@@ -9,5 +10,12 @@ routes.get("/customers/:id", customers.show);
 routes.post("/customers", customers.create);
 routes.put("/customers/:id", customers.update);
 routes.delete("/customers/:id", customers.destroy);
+
+// Contacts
+routes.get("/customers/:customerId/contacts", contacts.index);
+routes.get("/customers/:customerId/contacts/:id", contacts.show);
+routes.post("/customers/:customerId/contacts", contacts.create);
+routes.put("/customers/:customerId/contacts/:id", contacts.update);
+routes.delete("/customers/:customerId/contacts/:id", contacts.destroy);
 
 export default routes;
