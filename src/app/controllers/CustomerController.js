fad40e7bf92146576b1a8eb80b9eed593a233447
juarezjunior256 <juarezjunior.live@gmail.com>
2021-08-22 21:12:29 -0300

    CurstomerController index criado

--- src/app/controllers/CustomerController.js
+++ src/app/controllers/CustomerController.js
@@ -1,26 +1,110 @@
+import { Op } from "sequelize";
+import { parseISO } from "date-fns";
+
 import Customer from "../models/Customer";
+import Contact from "../models/Contact";
 
 class CustomerController {
   async index(req, res) {
+    const {
+      name,
+      email,
+      status,
+      createdBefore,
+      createdAfter,
+      updatedBefore,
+      updatedAfter,
+      sort,
+    } = req.query;
+
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 25;
+
+    let where = {};
+    let order = [];
+
+    if (name) {
+      where = {
+        ...where,
+        name: {
+          [Op.iLike]: name,
+        },
+      };
+    }
+
+    if (email) {
+      where = {
+        ...where,
+        email: {
+          [Op.iLike]: email,
+        },
+      };
+    }
+
+    if (status) {
+      where = {
+        ...where,
+        status: {
+          [Op.in]: status.split(",").map((item) => item.toUpperCase()),
+        },
+      };
+    }
+
+    if (createdBefore) {
+      where = {
+        ...where,
+        createdAt: {
+          [Op.gte]: parseISO(createdBefore),
+        },
+      };
+    }
+
+    if (createdAfter) {
+      where = {
+        ...where,
+        createdAt: {
+          [Op.lte]: parseISO(createdAfter),
+        },
+      };
+    }
+
+    if (updatedBefore) {
+      where = {
+        ...where,
+        updatedAt: {
+          [Op.gte]: parseISO(updatedBefore),
+        },
+      };
+    }
+
+    if (updatedAfter) {
+      where = {
+        ...where,
+        updatedAt: {
+          [Op.lte]: parseISO(updatedAfter),
+        },
+      };
+    }
+
+    if (sort) {
+      order = sort.split(",").map((item) => item.split(":"));
+    }
+
     const data = await Customer.findAll({
-      limit: 1000,
+      where,
+      include: [
+        {
+          model: Contact,
+          attributes: ["id", "status"],
+        },
+      ],
+      order,
+      limit,
+      offset: limit * page - limit,
     });
 
     return res.json(data);
   }
-
-  // show(req, res) {
-  //   // const id = parseInt(req.params.id);
-  //   // const customer = customers.find((item) => item.id === id);
-  //   // const status = customer ? 200 : 404;
-  //   // return res.status(status).json(customer);
-  // }
-
-  // create(req, res) {}
-
-  // update(req, res) {}
-
-  // destroy(req, res) {}
 }
 
 export default new CustomerController();
